a0bd4dea2eea51b1b4254e97f6b191cae1bbb7d3
LucasCelayes <fabian_celayes@hotmail.com>
2024-04-22 17:11:58 -0300

    clase.1

---
+++ clase_1/clase_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string nombre;
+nombre = "Lucas";
+
+const int dni = 52300042;
+
+Console.WriteLine("Mi nombre es: " + nombre);
+
+
+string apellido = "Celayes";
+
+Console.WriteLine("Mi apellido es:" + apellido);
+
+Console.WriteLine("mi dni es: " + dni);
